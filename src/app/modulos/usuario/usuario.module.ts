8b7810700e9b7057535f0f907420f0dbe93ff7ff
cristhian gonzalez <cristhianmg1122@gmail.com>
2022-03-12 23:16:22 -0500

    Se crearon los componentes parametrizacion

--- src/app/modulos/usuario/usuario.module.ts
+++ src/app/modulos/usuario/usuario.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsuarioRoutingModule } from './usuario-routing.module';
+import { CrearUsuarioComponent } from './crear-usuario/crear-usuario.component';
+import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
+import { BuscarUsuarioComponent } from './buscar-usuario/buscar-usuario.component';
+import { EliminarUsuarioComponent } from './eliminar-usuario/eliminar-usuario.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CrearUsuarioComponent,
+    EditarUsuarioComponent,
+    BuscarUsuarioComponent,
+    EliminarUsuarioComponent
+  ],
   imports: [
     CommonModule,
     UsuarioRoutingModule
